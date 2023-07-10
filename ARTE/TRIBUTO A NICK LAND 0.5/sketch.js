d2726ad719fd6293b96ae604b2a025050a95833a
Pajan <d47708394@alumnos.itr.edu.ar>
2023-07-09 21:10:30 -0600

    primer commit 9

---
+++ ARTE/TRIBUTO A NICK LAND 0.5/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for(let i = 0 ;i<800;i++){
+  line(random(400), random(400), random(400), random(400));
+  
+      }
+
+}
